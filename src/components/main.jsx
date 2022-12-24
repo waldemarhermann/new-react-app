0ad396cc4fafe5405b810b255548cf5ef2d5f1b7
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2022-12-24 13:56:43 +0100

    setup gh-pages

---
+++ src/components/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Main() {
+    return (
+        <main className="main">
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, inlcuding mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+
+export default Main
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <img src="./reactjs-icon.png" alt="png" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+export default Navbar
